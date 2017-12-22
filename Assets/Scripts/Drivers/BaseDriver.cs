06627b9d8a8a40c21719657d9b1e9c3434901bfb
Autofire <autofire555@gmail.com>
2017-12-21 21:10:15 -0800

    Began work on Driver system.

---
+++ Assets/Scripts/Drivers/BaseDriver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Drivers {
+
+	/// <summary>
+	/// This is the basic driver, which determines how characters behave. It can either be with input from a user or
+	/// based on AI.
+	///
+	/// Note that not all actions 
+	/// </summary>
+	public abstract class BaseDriver : MonoBehaviour {
+
+		#region Types
+
+		/// <summary>
+		/// Events of this type let other objects know when a command is given.
+		///
+		/// Returns true when the action is successful (i.e. nothing stopped it from being carried out).
+		/// </summary>
+		public delegate bool Command();
+
+		/// <summary>
+		/// Events of this type let other objects know when a command is given. These commands have some magnitude
+		/// assitiated with them.
+		///
+		/// Returns true when the action is successful (i.e. nothing stopped it from being carried out).
+		/// </summary>
+		public delegate bool MagnitudeCommand(float magnitude);
+
+		#endregion
+
+
+		/// <summary>
+		/// Called when driver wants to move in the X direction. Note that this will get called with a value of zero
+		/// whenever no movement is desired.
+		/// </summary>
+		public event MagnitudeCommand OnMoveX;
+
+		/// <summary>
+		/// Called when driver wants to move in the Y direction. Note that this will get called with a value of zero
+		/// whenever no movement is desired. 
+		/// </summary>
+		public event MagnitudeCommand OnMoveY;
+
+		public event Command OnJump;
+
+
+			
+	}
+}
---
+++ Assets/Scripts/General/Mover.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	[SerializeField] Vector3 moveAmount;
+	[SerializeField] CharacterController charController;
+
+	void FixedUpdate () {
+		if(charController != null) {
+
+			if(charController.isGrounded) {
+				charController.SimpleMove(moveAmount);
+			}
+			else {
+				charController.SimpleMove(Vector3.zero);
+			}
+
+			/*
+			CollisionFlags collisionFlags = charController.Move(moveAmount);
+
+			if (collisionFlags == CollisionFlags.None)
+				print("Free floating!");
+
+			if ((collisionFlags & CollisionFlags.Sides) != 0)
+				print("Touching sides!");
+
+			if (collisionFlags == CollisionFlags.Sides)
+				print("Only touching sides, nothing else!");
+
+			if ((collisionFlags & CollisionFlags.Above) != 0)
+				print("Touching sides!");
+
+			if (collisionFlags == CollisionFlags.Above)
+				print("Only touching Ceiling, nothing else!");
+
+			if ((collisionFlags & CollisionFlags.Below) != 0)
+				print("Touching ground!");
+
+			if (collisionFlags == CollisionFlags.Below)
+				print("Only touching ground, nothing else!");
+				*/
+		}
+		else {
+			transform.position = transform.position + moveAmount;
+		}
+
+	}
+}
