32f2bfe7f514543e9239f2ae0d14a576c4a48854
Autofire <autofire555@gmail.com>
2017-12-22 17:22:09 -0800

    Added more to the driver.

--- Assets/Scripts/Drivers/BaseDriver.cs
+++ Assets/Scripts/Drivers/BaseDriver.cs
@@ -33,21 +33,90 @@ namespace Drivers {
 		#endregion
 
 
+		#region Events
+
 		/// <summary>
 		/// Called when driver wants to move in the X direction. Note that this will get called with a value of zero
 		/// whenever no movement is desired.
+		/// 
+		/// This gets called on FixedUpdate.
 		/// </summary>
 		public event MagnitudeCommand OnMoveX;
 
 		/// <summary>
 		/// Called when driver wants to move in the Y direction. Note that this will get called with a value of zero
-		/// whenever no movement is desired. 
+		/// whenever no movement is desired.
+		///
+		/// This gets called on FixedUpdate.
 		/// </summary>
 		public event MagnitudeCommand OnMoveY;
 
-		public event Command OnJump;
+		#endregion
 
 
-			
+		#region Unity messages
+
+		protected virtual void FixedUpdate() {
+			IssueCommand(OnMoveX, 0f);
+			IssueCommand(OnMoveY, 0f);
+		}
+
+		#endregion
+
+
+		#region Command helpers
+
+		// TODO see about making a generic, non-class templated function to do this
+
+		/// <summary>
+		/// Fires all the given command to all subscribers. This is safe to call even if commandDelegate is null.
+		/// </summary>
+		/// <returns>
+		/// The array of results; for each listener that is called, a true or false is returned, depending on
+		/// whether the listener was successful in carrying out the command. If null is passed in, then an empty array
+		/// is returned.
+		/// </returns>
+		/// <param name="commandDelegate">The delegate to fire. This can be null or multicase.</param>
+		protected bool[] IssueCommand(Command commandDelegate) {
+
+			// Would use an array, but it's a pain to call 
+			List<bool> results = new List<bool>(commandDelegate.GetInvocationList().Length);
+
+			if(commandDelegate != null) {
+
+				foreach (Command cmd in commandDelegate.GetInvocationList()) {
+					results.Add(cmd());
+				}
+			}
+
+			return results.ToArray();
+		}
+
+		/// <summary>
+		/// Fires all the given command to all subscribers. This is safe to call even if commandDelegate is null.
+		/// </summary>
+		/// <returns>
+		/// The array of results; for each listener that is called, a true or false is returned, depending on
+		/// whether the listener was successful in carrying out the command. If null is passed in, then an empty array
+		/// is returned.
+		/// </returns>
+		/// <param name="commandDelegate">The delegate to fire. This can be null or multicase.</param>
+		/// <param name="magnitude">The magnitude to pass to the command functions.</param> 
+		protected bool[] IssueCommand(MagnitudeCommand commandDelegate, float magnitude) {
+
+			// Would use an array, but it's a pain to call 
+			List<bool> results = new List<bool>(commandDelegate.GetInvocationList().Length);
+
+			if(commandDelegate != null) {
+
+				foreach (MagnitudeCommand cmd in commandDelegate.GetInvocationList()) {
+					results.Add(cmd(magnitude));
+				}
+			}
+
+			return results.ToArray();
+		}
+
+		#endregion
 	}
 }
