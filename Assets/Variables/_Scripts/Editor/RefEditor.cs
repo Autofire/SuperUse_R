752b9bf0d7e27ddeb33d28caf0de39e049c11f69
Autofire <autofire555@gmail.com>
2018-01-02 22:18:44 -0800

    RefEditor now uses UnityEngine.TooltipAttribute.

--- Assets/Variables/_Scripts/Editor/RefEditor.cs
+++ Assets/Variables/_Scripts/Editor/RefEditor.cs
@@ -2,15 +2,18 @@
 using UnityEditor;
 using VariableObjects;
 
-/*
-public class RefEditor<Types, VarType> : PropertyDrawer 
-	where VarType: Variable<Types> {
-*/
 [CustomPropertyDrawer(typeof(LayerMaskConstReference))]
 [CustomPropertyDrawer(typeof(LayerMaskReference))]
+
+[CustomPropertyDrawer(typeof(FloatConstReference))]
+[CustomPropertyDrawer(typeof(FloatReference))]
 public class RefEditor : PropertyDrawer {
 
 	override public void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+
+		const string USE_INTERNAL_TOOLTIP =
+			"Checked: Use an 'internal' value; that is, one specified here.\n" +
+			"Unchecked: Use a variable asset.";
 
 		string valuePropertyName;
 
@@ -21,18 +24,43 @@ public class RefEditor : PropertyDrawer {
 			valuePropertyName = "variable";
 		}
 
+		label.tooltip = GetTooltip(fieldInfo);
+
+
+
 		EditorGUI.BeginProperty(position, label, property);
 
-
-		position = EditorGUI.PrefixLabel (position, GUIUtility.GetControlID (FocusType.Passive), label);
-
+		position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID (FocusType.Passive), label);
 
 		Rect checkBoxRect = new Rect(position.x, position.y, 10, position.height);
 		Rect fieldRect = new Rect(position.x+15, position.y, position.width - 15, position.height);
 
-		EditorGUI.PropertyField(checkBoxRect, property.FindPropertyRelative("_useInternal"), GUIContent.none);
-		EditorGUI.PropertyField(fieldRect, property.FindPropertyRelative(valuePropertyName), GUIContent.none);
+		EditorGUI.PropertyField( checkBoxRect, property.FindPropertyRelative("_useInternal"), GUIContent.none );
+		EditorGUI.PropertyField( fieldRect, property.FindPropertyRelative(valuePropertyName), GUIContent.none );
+
+		EditorGUI.LabelField(checkBoxRect, new GUIContent("", USE_INTERNAL_TOOLTIP));
 
 		EditorGUI.EndProperty();
-	}
-}
+	} // End OnGUI
+
+
+
+
+	string GetTooltip(System.Reflection.FieldInfo info) {
+		// Partial credit to https://answers.unity.com/answers/1421384/view.html
+
+		string tooltip = "";
+
+		object[] attributes = info.GetCustomAttributes(typeof(TooltipAttribute), true);
+
+		if(attributes.Length > 0) {
+			TooltipAttribute tt = attributes[0] as TooltipAttribute;
+			if (tt != null) {
+				tooltip = tt.tooltip;
+			}
+		}
+
+		return tooltip;
+	} // End GetTooltip
+
+} // End RefEditor
