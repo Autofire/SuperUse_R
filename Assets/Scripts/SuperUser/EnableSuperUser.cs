0f24397397cde858cf5eebbbb0af9171ed8b0c7a
Autofire <autofire555@gmail.com>
2018-01-03 22:06:51 -0800

    Made the VR system automatically toggle itself.

---
+++ Assets/Scripts/SuperUser/EnableSuperUser.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+using System.Linq;
+
+public class EnableSuperUser : MonoBehaviour {
+
+	void Awake() {
+		string[] supportedDevices = XRSettings.supportedDevices.Skip(1).ToArray();
+		XRSettings.LoadDeviceByName(supportedDevices);
+	}
+
+	private void LateUpdate() {
+
+		if(XRSettings.loadedDeviceName == "OpenVR" && XRSettings.enabled == false) {
+			XRSettings.enabled = true;
+			SteamVR.enabled = true;
+
+			for(int i = 0; i < transform.childCount; i++) {
+				transform.GetChild(i).gameObject.SetActive(true);
+			}
+		}
+	}
+
+	private void OnDisable() {
+		for(int i = 0; i < transform.childCount; i++) {
+			transform.GetChild(i).gameObject.SetActive(false);
+		}
+
+		XRSettings.enabled = false;
+		SteamVR.enabled = false;
+	}
+}
