b8d10e07f51e3a6a855e5a0ed1822f3cc4d83c06
Autofire <autofire555@gmail.com>
2018-01-10 09:43:43 -0800

    Made the haptic feedback scale and feel nice when charging.

--- Assets/Scripts/SuperUser/VirusRespawner.cs
+++ Assets/Scripts/SuperUser/VirusRespawner.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Assertions;
 using VariableObjects;
 
 namespace SuperUser {
@@ -9,33 +8,69 @@ namespace SuperUser {
 
 		[SerializeField]
 		private FloatConstReference chargeTime;
-		[SerializeField]
-		private IntConstReference rampExponent;
 
 		[Space(10)]
-		[SerializeField]
-		private ViveControllerAssistantReference leftAssistant;
-		[SerializeField]
-		private ViveControllerAssistantReference rightAssistant;
+		[Header("Haptic feedback")]
+		[Tooltip("This should be something between 0 and 1")]
+		[SerializeField] private FloatConstReference maxChargeStrength;
+		[Tooltip("This determines how 'smooth' the charge feels.")]
+		[SerializeField] private IntConstReference rampExponent;
+		[Tooltip("Once the charge is maxed out, the vibration will oscillate for this long before returning to the normal amount.")]
+		[SerializeField] private FloatConstReference maxChargeAlertDuration;
+
+		[Space(10)]
+		[Header("Object setup")]
+		[SerializeField] private VirusRespawner otherRespawner;
+		[SerializeField] private TransformConstReference respawnTransform;
+		[SerializeField] private ViveControllerAssistantReference assistant;
 
 		float chargeStartTime;
 		bool isCharging;
 
 		private void Awake() {
 			isCharging = false;
+
+			if(maxChargeStrength.constValue < 0 || maxChargeStrength.constValue > 1) {
+				Debug.LogWarning(
+					"You have maxChargeStrength in " + this.GetType().Name + " (attached to " + gameObject.name + ") "
+					+ "has a value of " + maxChargeStrength.constValue + " when a value between 0 and 1 was expected"
+				);
+			}
+		}
+
+		private void Start() {
+			Assert.IsNotNull(respawnTransform.constValue);
+			Assert.IsNotNull(assistant);
 		}
 
 		private void FixedUpdate() {
+			if(assistant.value.Controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+				BeginRespawnCharge();
+			}
+
+			if(assistant.value.Controller.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+				EndRespawnCharge();
+			}
+
 			if(isCharging) {
-				float charge = Mathf.Clamp01((Time.time - chargeStartTime) / chargeTime.constValue);
-				float rampedCharge = 1f;
+				if(Time.fixedTime - chargeStartTime > chargeTime.constValue && Time.fixedTime - chargeStartTime < chargeTime.constValue + maxChargeAlertDuration.constValue) {
+					// We've just hit the peak of the charge. We want to do a different kind of vibration.
 
-				for(int i = 0; i < rampExponent.constValue; i++) {
-					rampedCharge *= charge;
+					// We use PerlinNoise here because it gives a nice, consistent "rumble"
+					assistant.value.PulseVibration(Mathf.PerlinNoise(10 * (Time.fixedTime - chargeStartTime), 0f));
 				}
+				else {
+					// We're charging or have finished charging for a while now.
+					float charge = Mathf.Clamp01((Time.fixedTime - chargeStartTime) / chargeTime.constValue);
 
-				leftAssistant.value.PulseVibration(rampedCharge);
-				rightAssistant.value.PulseVibration(rampedCharge);
+					float rampedCharge = 1f;
+
+					for(int i = 0; i < rampExponent.constValue; i++) {
+						rampedCharge *= charge;
+					}
+
+					assistant.value.PulseVibration(Mathf.Min(rampedCharge, maxChargeStrength.constValue));
+				}
 			}
 		}
 
